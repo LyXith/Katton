85324b27a42b303105a4275bcb0351596d884adc
mikaple <3938307703@qq.com>
2026-09-23 03:30:49 +0800

    Update KattonBlockModificationApi.kt

--- common/src/main/kotlin/top/katton/api/mod/KattonBlockModificationApi.kt
+++ common/src/main/kotlin/top/katton/api/mod/KattonBlockModificationApi.kt
@@ -243,7 +243,7 @@ private fun applyBlockModifications(block: Block, config: BlockModificationConfi
     }
 
     config.isSuffocating?.let { isSuffocating ->
-        properties.isSuffocating(stateArgumentPredicate(isSuffocating))
+        properties.isSuffocating(statePredicate(isSuffocating))
     }
 
     config.isViewBlocking?.let { isViewBlocking ->
