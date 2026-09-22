f8233a63188e4c4f98a52f3acf600ff01f6a5279
mikaple <3938307703@qq.com>
2026-09-23 06:12:40 +0800

    Update EnderManMixin.java

--- fabric/src/main/java/top/katton/mixin/EnderManMixin.java
+++ fabric/src/main/java/top/katton/mixin/EnderManMixin.java
@@ -1,7 +1,7 @@
 package top.katton.mixin;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Enderman;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
