3c263880a270213e63a6ed20c5447dde9d2c2795
mikaple <3938307703@qq.com>
2026-09-23 11:49:42 +0800

    Update SceneKeyboardMixin.java

--- fabric/src/main/java/top/katton/mixin/SceneKeyboardMixin.java
+++ fabric/src/main/java/top/katton/mixin/SceneKeyboardMixin.java
@@ -1,8 +1,8 @@
 package top.katton.mixin;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.input.KeyEvent;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +13,6 @@ import top.katton.client.scene.ClientSceneManager;
 public abstract class SceneKeyboardMixin {
     @Inject(method = "keyPress", at = @At("HEAD"), cancellable = true)
     private void katton$skip(long window, int action, KeyEvent event, CallbackInfo ci) {
-        if (event.key() == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS && ClientSceneManager.skipCamera()) ci.cancel();
+        if (event.key() == InputConstants.KEY_ESCAPE && action == InputConstants.PRESS && ClientSceneManager.skipCamera()) ci.cancel();
     }
 }
