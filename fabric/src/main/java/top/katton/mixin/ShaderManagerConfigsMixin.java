2e1e32c4aeca998c393ac6775a50d74eaba03ddf
mikaple <3938307703@qq.com>
2026-09-23 12:08:13 +0800

    Create ShaderManagerConfigsMixin.java

---
+++ fabric/src/main/java/top/katton/mixin/ShaderManagerConfigsMixin.java
@@ -0,0 +1,20 @@
+package top.katton.mixin;
+
+import com.mojang.renderpearl.api.pipeline.ShaderType;
+import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.katton.client.ClientPostEffectManager;
+
+@Mixin(targets = "net.minecraft.client.renderer.ShaderManager$Configs")
+public abstract class ShaderManagerConfigsMixin {
+    @Inject(method = "getShader", at = @At("HEAD"), cancellable = true)
+    private void katton$getRuntimeShader(Identifier id, ShaderType type, CallbackInfoReturnable<String> cir) {
+        String source = ClientPostEffectManager.getRuntimeShaderSource(id, type);
+        if (source != null) {
+            cir.setReturnValue(source);
+        }
+    }
+}
